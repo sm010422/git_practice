a4d334a9fac02a796bdd89f4691ce032914403bd
Park Sang Min <88922405+sm010422@users.noreply.github.com>
2024-07-25 15:47:38 +0900

    git commit test2

--- practice_git.py
+++ practice_git.py
@@ -1,3 +1,6 @@
 print("this is practice git")
 print("this is commit test")
 print("this is commit test2")
+
+
+print("git commit test3")
--- practice_git2.py
+++ practice_git2.py
@@ -1,1 +1,2 @@
 print("this is git practice file 2")
+print("this is practice_git2 and git commit test3")
